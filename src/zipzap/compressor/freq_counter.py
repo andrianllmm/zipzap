4dfaf383f82d278ae2eee72f27dfd4e29413780b
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-12-08 13:19:24 +0800

    Simplify freq counter

--- src/zipzap/compressor/freq_counter.py
+++ src/zipzap/compressor/freq_counter.py
@@ -7,9 +7,5 @@ class FreqCounter(ProbeHashmap[str, int]):
     def __init__(self, text: str):
         super().__init__(256)
         for c in text:
-            if self.get(c) is None:
-                self.put(c, 1)
-            else:
-                current_count = self.get(c)
-                assert current_count is not None
-                self.put(c, current_count + 1)
+            current = self.get(c) or 0
+            self.put(c, current + 1)
