f55b56e2e82cd9af38eac3f1cc4260c26e9abfdd
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-12-09 09:42:15 +0800

    Update code lengths algo

--- src/zipzap/compressor/code_lengths.py
+++ src/zipzap/compressor/code_lengths.py
@@ -11,8 +11,7 @@ def compute_code_lengths(tree: HuffmanTree) -> Map[str, int]:
         if node.element().char is not None:
             code_lengths.put(node.element().char, depth)
             return
-        children = list(tree.children(node))
-        for child in children:
+        for child in tree.children(node):
             dfs(child, depth + 1)
 
     if not tree.is_empty():
